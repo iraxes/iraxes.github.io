33e9bffa49bd5896ebbc650df97d99b56b1a347e
iraxes <83122603+iraxes@users.noreply.github.com>
2021-04-25 15:27:44 +0300

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,75 @@
+ideologies = [
+    {
+        "name": "Idealism",
+        "stats": {
+            "ag": 0,
+            "de": 0,
+            "na": 0,
+            "po": 50
+        }
+    },
+    {
+        "name": "Liberalism",
+        "stats": {
+            "ag": 10,
+            "de": 10,
+            "na": 30,
+            "po": 50
+        }
+    },
+    {
+        "name": "Neoliberalism",
+        "stats": {
+            "ag": 30,
+            "de": 20,
+            "na": 30,
+            "po": 40
+        }
+    },
+	{
+        "name": "Classic Realism",
+        "stats": {
+            "ag": 80,
+            "de": 100,
+            "na": 80,
+            "po": 100
+        }
+    },
+	{
+        "name": "Offencive Realism",
+        "stats": {
+            "ag": 60,
+            "de": 80,
+            "na": 60,
+            "po": 80
+        }
+    },
+    {
+        "name": "Defencive Realism",
+        "stats": {
+            "ag": 40,
+            "de": 60,
+            "na": 60,
+            "po": 80
+        }
+    },
+    {
+        "name": "Structuralism",
+        "stats": {
+            "ag": 20,
+            "de": 20,
+            "na": 50,
+            "po": 20
+        }
+    },
+    {
+        "name": "Rationalism",
+        "stats": {
+            "ag": 40,
+            "de": 40,
+            "na": 10,
+            "po": 80
+        }
+    },
+  
+];
---
+++ questions.js
@@ -0,0 +1,171 @@
+questions = 
+    [[//Offence-Defence`
+        [//4
+            "Security can only be achieved by maximizing power and hegemony at best",
+        ],
+        [//3
+            "In most cases, the initiator wins the war."
+        ],
+        [//2
+            "Alliance potential is overrated"    
+        ],
+        [//1
+            "The theory of international relations can do without the internal dimension of states. All states proceed from the same and simple premises"
+        ],
+        [//-1
+            "IR needs a clear theory of foreign policy. The behavior of states may not be strategic and may differ from actor to actor"
+        ],
+        [//-2
+            "In conditions of hegemony, the only one who is safe is this small state"
+        ],
+        [//-3
+            "Although the international system pushes states towards hegemony, it is more of a threat to security than a protection",
+        ],
+        [//-4
+            "The only fate of the hegemon is to be defeated.",
+        ]],[//Independence-Interdependence`
+        [//4
+            "Obviously, the states with the greatest military power will act as they please, while the weaker ones will not be able to oppose them"
+        ],
+        [//3
+            "The concepts of state power are largely limited to coercion arising from differences in material capabilities"
+        ],
+        [//2
+            "The less a state has a chance to act independently, the more it is underprotected"
+        ],
+        [//1
+            "No cost can stop a powerful enough state"
+        ],
+        [//-1
+            "Power largely consists of resources and dependencies; capabilities, however great they may be, may never be realized"
+        ],
+        [//-2
+             "The size of the state is not decisive in avoiding coercive influence"
+        ],
+        [//-3
+            "Military and economic power mean nothing if a state is put over a barrel by the structure of the international system"
+        ],
+        [//-4
+            "The more interdependent the system is, the safer it is"
+        ]],[//Irrationalist-Rrationalist`
+        [//4
+            "Conflicts can’t be overcome, as they are caused by the abiding impulses of human lust for power and reasonlessness"
+        ],
+        [//3
+            "States are struggling for maximum power in search of security. Their actions determine rational and selfish human attitudes"
+        ],
+        [//2
+            "International politics is dominated by human impulses and motives"
+        ],
+        [//1
+            "Any state as a rational actor can come to conclusions about the unreasonableness of cooperation"
+        ],
+        [//-1
+            "Sovereignty can be violated insofar as it is beneficial to everyone: those who interfere and those where former interfere"
+        ],
+        [//-2
+            "The formation of the agenda is based on maximizing utility"
+        ],
+        [//-3
+            "Power of states is determined by what motives are driving them, what information they possess and what material capabilities they have"
+        ],
+        [//-4
+            "Material capabilities aside, the most powerful state is the one that achieves the best results at the lowest cost. This would mean being fully informed.  "
+        ]],[//Securitizationist-Politizationist`
+        [//4
+            "National interests must be expanded without any political dialogue."
+        ],
+        [//3
+            "Issues of security and the balance of power are behind the shaping of the agenda"
+        ],
+        [//2
+            "Nation states can afford to break the law if the situation calls for it."
+        ],
+        [//1
+            "The more polar the world is, the less peaceful it is"
+        ],
+        [//-1
+            "Political is not always state, but state is always political"
+        ],
+        [//-2
+            "Social and economic issues or low politics are behind the shaping of the agenda"
+        ],
+        [//-3
+            "Any activity of states should become public knowledge and policy-making"
+        ],
+        [//-4
+            "One of the pressing issues of security is to prevent it from leaving the political field, freeing the actions of states from justification by the international community and civil society" 
+        ]],[//Statist-Individualist`
+        [//4
+            "The nation-state should be the only unit of analysis"
+        ],
+        [//3
+            "The categories of moral values are not applicable to states as they are applicable to individuals"
+        ],
+        [//2
+            "The behavior of states is determined not by the individual, butV by the very structure of international politics."
+        ],
+        [//1
+            "We can change the degree of anarchy of the system, but its incentives, and therefore the interests of states, cannot"
+        ],
+        [//-1
+            "The effectiveness of coercion cannot be described in terms of absolute loss of profits for a state"
+        ],
+        [//-2
+            "Democracies don't attack each other"
+        ],
+        [//-3
+            "States can proceed from their own principles, which cannot be reduced to such concepts as power or domination"
+        ],
+        [//-4
+            "It is wrong to consider the state in isolation from the individuals that form it and for the most of whom the conflict is disgusted"
+        ]],[//Actors-Structures`
+        [//4
+            "Power  is an attribute of states and nothing more"
+        ],
+        [//3
+            "The control of one state over another by means of influence: orders, threats or persuasion is the most obvious expression of power"
+        ],
+        [//2
+            "Any state of the international system is the result of a balance of power"
+        ],
+        [//1
+            "If states are not fully responsible for their policies, then it is useless to study international relations."
+        ],
+        [//-1
+            "Power is a strategic situation. It can be understood only by having information about the state of societies and the patterns they follow"
+        ],
+        [//-2
+            "Taming anarchy with the identity of the peace is the best way to achieve it"
+        ],
+        [//-3
+            "All values, both within and outside states, are relative, competing versions of truth. And the truth subordinates, here is its meaning in IR"
+        ],
+        [//-4
+            "Neither the state nor the individual can free itself from structural coercion; the international structure can thus be called a dictatorship rather than an anarchy"
+        ]],[//Nationalist-Internationalist`
+	    [//4
+            "An interest common to a certain number of states cannot exist"
+        ],
+        [//3
+            "The international system is in a conflict-free state insofar as it benefits and does not threaten powers great enough to support it"
+        ],
+        [//2
+            "Paying tribute to international organizations, the nation state however was and will be a unit of analysis outside of any form of world government"
+        ],
+        [//1
+            "The only option for cooperation is cooperation in the national interest"
+        ],
+        [//-1
+            "One should not even argue with the fact that sovereignty mean little in a globalized world"
+        ],
+        [//-2
+            "Cooperation is not unavoidable, but it increases when the state has the necessary institutions such as democracy and fairplay"
+        ],
+        [//-3
+            "The issues of political community and democracy have long gone beyond the rigid framework of national sovereignty. So why don't we give it up?"
+        ],
+        [//-4
+            "Cooperation in common interests, contrary to national, is possible and preferable",
+        ]
+    ]];
