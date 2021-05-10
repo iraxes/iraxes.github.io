e650b9cf5f8cf69e99edeca7e0d0499c6faa982a
iraxes <83122603+iraxes@users.noreply.github.com>
2021-05-10 20:12:52 +0300

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,171 +1,170 @@
 questions = 
     [[//Offence-Defence`
         [//4
-            "Security can only be achieved by maximizing power and hegemony at best",
+            "Security can only be achieved by maximizing power and hegemony at best.",
         ],
         [//3
-            "In most cases, the initiator wins the war."
+            "Initiator is almost a war winner.",
         ],
         [//2
-            "Alliance potential is overrated"    
+            "Alliance potential is overrated.",    
         ],
         [//1
-            "All states proceed from the same and simple premises"
+            "If there is such an opportunity, it is common for states to attack.",
         ],
         [//-1
-            "The behavior of states cannot be understood without understanding their political order"
+            "An internal politics cannot be understood without external.",
         ],
         [//-2
-            "In conditions of hegemony, the only one who is safe is this small state"
+            "Even the smallest state under the hegemon is safer than the hegemon itself.",
         ],
         [//-3
-            "Although the international system pushes states towards hegemony, it is more of a threat to security than a protection",
+            "Although the international system pushes states towards hegemony, it is more of a threat to security than a protection.",
         ],
         [//-4
-            "The only fate of the hegemon is to be defeated.",
+            "The hegemon is always defeated, how is this secure?"
         ]],[//Independence-Interdependence`
         [//4
-            "Obviously, the states with the greatest military power will act as they please, while the weaker ones will not be able to oppose them"
+            "Obviously, the states with the greatest military power will act as they please, while the weaker ones will not be able to oppose them.",
         ],
         [//3
-            "The concepts of state power are largely limited to coercion arising from differences in material capabilities"
+            "The concepts of state power are largely limited to coercion arising from differences in material capabilities.",
         ],
         [//2
-            "The less a state has a chance to act independently, the more it is underprotected"
+            "The less a state has a chance to act independently, the more it is underprotected.",
         ],
         [//1
-            "No cost can stop a powerful enough state"
+            "No cost can stop a powerful enough state.",
         ],
         [//-1
-            "Power largely consists of dependencies; capabilities, however great they may be, may never be realized"
+            "Power largely consists of dependencies; capabilities, however great they may be, may never be realized.",
         ],
         [//-2
-             "The size of the state is not decisive in avoiding coercive influence"
+             "The size of the state is not decisive in avoiding coercive influence.",
         ],
         [//-3
-            "Military and economic power mean nothing if a state is put over a barrel by the structure of the international system"
+            "Military and economic power mean nothing if a state is put over a barrel by the structure of the international system."
         ],
         [//-4
-            "The more interdependent the system is, the safer it is"
-        ]],[//Irrationalist-Rrationalist`
+            "The more interdependent the system is, the safer it is.",
+        ]],[//Irrationalism-Rationalism`
         [//4
-            "Conflicts can’t be overcome, as they are caused by the abiding impulses of human lust for power and reasonlessness"
+            "Conflicts can’t be overcome, as they are caused by the impulses of human lust for power.",
         ],
         [//3
-            "States' actions determine human attitudes"
+            "A state without resources is nothing.",
         ],
         [//2
-            "International politics is dominated by human impulses and motives"
+            "No matter how democratic states are, their actions are never directly dictated by the people.",
         ],
         [//1
-            "Any state as a rational actor can come to conclusions about the unreasonableness of cooperation"
+            "Any state as a rational actor can come to conclusions about the unreasonableness of cooperation.",
         ],
         [//-1
-            "Sovereignty can be violated insofar as it is beneficial to everyone"
+            "Sovereignty can be violated insofar as it is beneficial to everyone.",
         ],
         [//-2
-            "The formation of the agenda is based on maximizing utility"
+            "The agenda-setting is based on maximizing utility.",
         ],
         [//-3
-            "Power of states is determined by what motives are driving them, what information they possess and what material capabilities they have"
+            "3 factors driving states: motives, information, capabilities.",
         ],
         [//-4
-            "The most powerful state is the one that achieves the best results at the lowest cost"
-        ]],[//Securitizationist-Politizationist`
+            "The most powerful usually means the most informed." 
+        ]],[//Securitization-Politization`
         [//4
-            "National interests must be expanded without any political dialogue."
+            "National interests must be expanded, out of the question.",
         ],
         [//3
-            "Issues of security and the balance of power are behind the shaping of the agenda"
+            "Issues of security and the balance of power are behind the agenda-setting.",
         ],
         [//2
-            "Nation states can afford to break the law if the situation calls for it."
+            "Nation states can afford to break the law if the situation calls for it.",
         ],
         [//1
-            "The more polar the world is, the less peaceful it is"
+            "The more polar the world is, the less peaceful it is."
         ],
         [//-1
-            "Political is not always state, but state is always political"
+            "Political is not always a public, but everything that is public must be the object of politics.",
         ],
         [//-2
-            "Social and economic issues or low politics are behind the shaping of the agenda"
+            "Social and economic issues are behind the agenda-setting."
         ],
         [//-3
-            "Any activity of states should become public knowledge and policy-making"
+            "Any activity of state should be open for debate."
         ],
         [//-4
-            "The main issue of security is to prevent it evasion from evaluation by the international community and civil society" 
-        ]],[//Statist-Individualist`
+            "The main security issue is to keep security from engulfing all the political debates."
+        ]],[//Statism-Individualism`
         [//4
-            "The nation-state should be the only unit of analysis"
-        ],
+            "The nation-state should be the only unit of analysis.",        ],
         [//3
-            "The categories of moral values are not applicable to states as they are applicable to individuals"
+            "The categories of moral values are not applicable to states.",
         ],
         [//2
-            "The behavior of states is determined not by the individual, but the very structure of international politics"
+            "States’ actions determined not by the individuals, but the very structure of international politics",
         ],
         [//1
-            "We can reduce the degree of anarchy of the system, but we cannot force states to abandon their interests"
+            "We can reduce the degree of anarchy, but cannot force states to abandon their interests.",
         ],
         [//-1
-            "The effectiveness of coercion cannot be described in terms of absolute loss of profits for a state"
+            "The effectiveness of coercion cannot be described as absolute loss for a state.",
         ],
         [//-2
-            "Democracies don't attack each other"
+            "Democracies don't attack each other.",
         ],
         [//-3
-            "States can proceed from their own principles, which cannot be reduced to such concepts as power or domination"
+            "States can proceed from their own principles, which cannot be reduced to such concepts as power or domination.",
         ],
         [//-4
-            "It is wrong to consider the state in isolation from the individuals"
+            "It is wrong to consider the state in isolation from the individuals."
         ]],[//Actors-Structures`
         [//4
-            "Power  is an attribute of states and nothing more"
+            "Power is an attribute of states and nothing more.",
         ],
         [//3
-            "The control of one state over another by means of influence: orders, threats or persuasion is the most obvious expression of power"
+            "The control of one state over another by means of influence: orders, threats or persuasion is the most obvious expression of power.",
         ],
         [//2
-            "Any state of the international system is the result of a balance of power"
+            "Any state of the international system is the result of a balance of power.",
         ],
         [//1
-            "If states are not fully responsible for their policies, then it is useless to study international relations"
+            "If there is something other than the states themselves responsible for their actions, then it is useless to study politics.", 
         ],
         [//-1
-            "Power can be understood only by having information about the state of societies and the patterns they follow"
+            "Power can be understood only as we aware of the societies and the patterns they follow.",
         ],
         [//-2
-            "Taming anarchy with the identity of the peace is the best way to achieve it"
+            "Taming anarchy with the identity of the peace is the best way to achieve it.",
         ],
         [//-3
-            "All values, both within and outside states, are relative, competing versions of truth"
+            "All values, both within and outside states, are relative, competing versions of truth.",
         ],
         [//-4
-            "One must call the international structure a dictatorship: neither the state nor the individual can free itself from structural coercion"
-        ]],[//Nationalist-Internationalist`
+            "IR is rather a dictatorship: neither the state nor the individual can free itself from structural coercion."
+        ]],[//Nationalism-Internationalism`
 	    [//4
-            "An interest common to a certain number of states cannot exist"
+            "An interest common to a certain number of states cannot exist.",
         ],
         [//3
-            "The international system is in a conflict-free state insofar as it benefits and does not threaten powers great enough to support it"
+            "The international system is conflict-free insofar as it benefits powers great enough to support it.",
         ],
         [//2
-            "The nation state was and will be a unit of analysis outside of any form of world government"
+            "A world government is rather a bad idea.",
         ],
         [//1
-            "The only option for cooperation is cooperation in the national interest"
+            "The only option for cooperation is cooperation in the national interest.",
         ],
         [//-1
-            "One should not even argue with the fact that sovereignty mean little in a globalized world"
+            "One should not even argue with the fact that sovereignty mean little in a globalized world.",
         ],
         [//-2
-            "Cooperation is not unavoidable, but it increases when the state has the necessary institutions such as democracy and fairplay"
+            "Cooperation is not unavoidable, but increases when the state supports institutions such as democracy and fairplay.",
         ],
         [//-3
-            "Sovereignty can no longer be a matter of states, just as democracy or political community ceased to be"
+            "Sovereignty can no longer be a matter of states.",
         ],
         [//-4
-            "Cooperation in common interests, contrary to national, is possible and preferable"
+            "Cooperation in common interests, contrary to national, is possible and preferable."
         ]
     ]];
